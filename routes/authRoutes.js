5c71e419e14c3e0fe368ef1632fed3c6d9a163ff
Israa-Abuelaish <ibasheer264@gmail.com>
2026-09-09 21:03:38 +0200

    Update register route with error logging and fix customerProfile creation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,9 +7,8 @@ const jwt = require('jsonwebtoken');
 
 // 1. تسجيل حساب جديد (للزبائن فقط)
 router.post('/register', async (req, res) => {
-  console.log("🔥 تم استقبال طلب تسجيل جديد!", req.body); 
   try {
-    const { name, email, password, phone, address } = req.body;
+    const { name, email, password, phone, address } = req.body; // استقبال الـ phone
 
     const existingUser = await prisma.user.findUnique({ where: { email } });
     if (existingUser) {
@@ -27,7 +26,7 @@ router.post('/register', async (req, res) => {
         role: 'customer',
         customerProfile: {
           create: {
-            address: address 
+            address 
           }
         }
       },
@@ -36,7 +35,6 @@ router.post('/register', async (req, res) => {
 
     res.status(201).json({ message: 'تم إنشاء الحساب بنجاح', user: newUser });
   } catch (error) {
-    console.error("Register Error:", error); 
     res.status(500).json({ error: error.message });
   }
 });
